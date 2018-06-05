981c8913df538d3dd7434bdd69f078c3bd63424d
Peter Lee <peter@TEE63.WPA.Dal.Ca>
2018-06-04 22:23:16 -0300

    Renamed elements in tests to match UI

--- app/src/androidTest/java/com/example/peter/passwordvalidator/InstrumentTestValidator.java
+++ app/src/androidTest/java/com/example/peter/passwordvalidator/InstrumentTestValidator.java
@@ -1,12 +1,36 @@
+package com.example.peter.passwordvalidator;
+
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.peter.passwordvalidator.MainActivity;
+import com.example.peter.passwordvalidator.R;
+
 import org.junit.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
-//import Validator.*;
-//import static Validator.checkPassword;
-public class TestValidator{
-    
+
+@RunWith(AndroidJUnit4.class)
+public class InstrumentTestValidator{
+
+
+	@Rule
+	public ActivityTestRule<MainActivity> activityrule =
+			new ActivityTestRule<>(MainActivity.class);
     @Test
     public void testCheckPassword() {
 	/* Test different combination for passwords */
@@ -20,35 +44,43 @@ public class TestValidator{
 	String pass5 = "HelloThere0^";
 
 
-	onView((R.id.password)).perform(typeText(pass1a));
-	onView(withId(R.id.submit)).perform(click());
+	onView(withId(R.id.password)).perform(clearText(), typeText(pass1a));
+	closeSoftKeyboard();
+	onView(withId(R.id.validate)).perform(click());
+	onView(withId(R.id.strength_message)).check(matches(withText("very weak")));
+
+	onView(withId(R.id.password)).perform(clearText(), typeText(pass1b));
+	closeSoftKeyboard();
+	onView(withId(R.id.validate)).perform(click());
+	onView(withId(R.id.strength_message)).check(matches(withText("very weak")));
 	onView(withText("very weak")).check(matches(isDisplayed()));
 
-	onView((R.id.password)).perform(typeText(pass1b));
-	onView(withId(R.id.submit)).perform(click());
-	onView(withText("very weak")).check(matches(isDisplayed()));
-
-	onView((R.id.password)).perform(typeText(pass1c));
-	onView(withId(R.id.submit)).perform(click());
-	onView(withText("very weak")).check(matches(isDisplayed()));
+	onView(withId(R.id.password)).perform(clearText(), typeText(pass1c));
+	closeSoftKeyboard();
+	onView(withId(R.id.validate)).perform(click());
+	onView(withId(R.id.strength_message)).check(matches(withText("very weak")));
 
 
-	onView((R.id.password)).perform(typeText(pass2));
-	onView(withId(R.id.submit)).perform(click());
-	onView(withText("very weak")).check(matches(isDisplayed()));
+	onView(withId(R.id.password)).perform(clearText(), typeText(pass2));
+	closeSoftKeyboard();
+	onView(withId(R.id.validate)).perform(click());
+	onView(withId(R.id.strength_message)).check(matches(withText("very weak")));
 
-	onView((R.id.password)).perform(typeText(pass3));
-	onView(withId(R.id.submit)).perform(click());
-	onView(withText("medium")).check(matches(isDisplayed()));
+	onView(withId(R.id.password)).perform(clearText(), typeText(pass3));
+	closeSoftKeyboard();
+	onView(withId(R.id.validate)).perform(click());
+	onView(withId(R.id.strength_message)).check(matches(withText("medium")));
 
-	onView((R.id.password)).perform(typeText(pass4));
-	onView(withId(R.id.submit)).perform(click());
-	onView(withText("medium")).check(matches(isDisplayed()));
+	onView(withId(R.id.password)).perform(clearText(), typeText(pass4));
+	closeSoftKeyboard();
+	onView(withId(R.id.validate)).perform(click());
+	onView(withId(R.id.strength_message)).check(matches(withText("medium")));
 
 
-	onView((R.id.password)).perform(typeText(pass5));
-	onView(withId(R.id.submit)).perform(click());
-	onView(withText("strong")).check(matches(isDisplayed()));
+	onView(withId(R.id.password)).perform(clearText(), typeText(pass5));
+	closeSoftKeyboard();
+	onView(withId(R.id.validate)).perform(click());
+	onView(withId(R.id.strength_message)).check(matches(withText("strong")));
 
     }
 }
